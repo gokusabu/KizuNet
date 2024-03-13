4fe7075b6844454210e25a8008b20f9b937d0282
G0kuuu <gokulsabu02@gmail.com>
2024-03-13 21:10:18 +0530

    Added top bar containing search and create post button

---
+++ app/components/layout/MainContainer.jsx
@@ -0,0 +1,12 @@
+
+import TopBar from './TopBar'
+
+const MainContainer = ({children}) => {
+  return (
+    <section className='flex flex-col flex-1 max-w-3xl px-4 md:px-10 lg:px-4 xl-px-20'>
+      <TopBar/>
+    </section>
+  )
+}
+
+export default MainContainer
