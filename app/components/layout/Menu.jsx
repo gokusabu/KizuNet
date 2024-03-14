af5c43de377b2db730826c448973df5fba5b946f
G0kuuu <gokulsabu02@gmail.com>
2024-03-14 20:29:21 +0530

    bottom bar menu and right side bar

--- app/components/layout/Menu.jsx
+++ app/components/layout/Menu.jsx
@@ -12,7 +12,7 @@ const Menu = () => {
             const isActive = pathname === link.route
 
             return(
-                <Link key={link.label} href={link.route} className={`flex start-4 justify-start rounded-lg py-2 px-4 ${isActive && "bg-purple-1"}`}>
+                <Link key={link.label} href={link.route} className={`flex gap-4 justify-start rounded-lg py-2 px-4 ${isActive && "bg-purple-1"}`}>
                 {link.icon}<p className="text-light-1">{link.label}</p>
                 </Link>
             )
