7381f44c2d8c90c576833bb202d22a2cc33d5502
G0kuuu <gokulsabu02@gmail.com>
2024-04-03 21:04:08 +0530

    postCard complete

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,9 +1,13 @@
 "use client"
 import Loader from '@app/components/Loader'
 import PostCard from '@app/components/cards/PostCard'
+import { useUser } from '@clerk/nextjs'
 import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+
+  const {user , isLoaded} = useUser()
+
   const [loading,setLoading] = useState(true)
 
   const [feedPost,setFeedPost] = useState([])
@@ -19,11 +23,16 @@ const Home = () => {
     getFeedPost()
   },[])
   
-  return loading ? <Loader/> :(
+  return loading || !isLoaded ? <Loader/> :(
     <div className='flex flex-col gap-10'>
-      {feedPost.map((post)=>{
-        <PostCard/>
-      })}
+      {feedPost.map((post)=>(
+        <PostCard
+        key={post._id}
+        post={post}
+        creator={post.creator}
+        loggedInUser={user}
+        />
+      ))}
     </div>
   )
 }
