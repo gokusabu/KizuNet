79b9d04334ce9e4bab7ce2e3f295bf720c74d74c
G0kuuu <gokulsabu02@gmail.com>
2024-04-06 19:07:04 +0530

    posts in user profile

--- app/api/user/profile/[profileId]/route.js
+++ app/api/user/profile/[profileId]/route.js
@@ -1,10 +1,25 @@
+import Post from "@lib/models/Post"
 import User from "@lib/models/User"
 import { connectToDB } from "@lib/mongodb/mongoose"
 
 export const GET = async(req,{params}) =>{
     try{
         await connectToDB()
-        const user = await User.findById(params.profileId).populate("posts likedPosts savedPosts followers following").exec()
+        const user = await User.findById(params.profileId).populate({
+            path:"posts likedPosts savedPosts",
+            model:Post,
+            populate:{
+                path:"creator",
+                model:User
+            }
+        }).populate({
+            path:"followers following",
+            model:Post,
+            populate:{
+                path:"posts likedPosts savedPosts",
+                model:Post
+            }
+        }).exec()
         return new Response(JSON.stringify(user),{status:200})
     }catch(err){
         return new Response("Error fetching the user",{status:500})
