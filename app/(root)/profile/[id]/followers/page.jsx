83ee6a880eece326b269c15a11a7df9f91935b03
G0kuuu <gokulsabu02@gmail.com>
2024-04-08 12:14:22 +0530

    followers page and folow bug fix

--- app/(root)/profile/[id]/followers/page.jsx
+++ app/(root)/profile/[id]/followers/page.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+"use client"
+import UserCard from '@app/components/cards/UserCard'
+import Loader from '@app/components/Loader'
+import ProfileCard from '@app/components/cards/ProfileCard'
+import { useParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
 
 const Followers = () => {
-  return (
-    <div>Followers</div>
+  const { id }= useParams()
+
+  const [loading,setLoading] = useState(true)
+
+  const [userData,setUserData] = useState({})
+
+  const getUser = async()=>{
+    const response = await fetch(`/api/user/profile/${id}`)
+    const data = await response.json()
+    setUserData(data)
+    setLoading(false)
+  }
+  useEffect(()=>{
+    getUser()
+  },[id])
+
+  return loading ? <Loader/> : (
+    <div className='flex flex-col gap-9'>
+      <ProfileCard userData={userData} activeTab='Followers' />
+      <div className='flex flex-col gap-9'>
+        {userData?.followers?.map((person)=>(
+          <UserCard key={person._id} userData={person} update={getUser}/>
+        ))}
+
+      </div>
+    </div>
+    
   )
 }
 
--- app/components/cards/UserCard.jsx
+++ app/components/cards/UserCard.jsx
@@ -1,72 +1,90 @@
-"use client"
-import { useUser } from '@clerk/nextjs'
-import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
-import Loader from '../Loader'
-import { PersonAddAlt, PersonRemove } from '@mui/icons-material'
+"use client";
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import Loader from "../Loader";
+import { PersonAddAlt, PersonRemove } from "@mui/icons-material";
 
-const UserCard = ({userData , update}) => {
-    const {user , isLoaded} = useUser()
+const UserCard = ({ userData, update }) => {
+  const { user, isLoaded } = useUser();
 
-    const [loading,setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
-    const[userInfo,setUserInfo] =useState({})
+  const [userInfo, setUserInfo] = useState({});
 
-    const getUser = async()=>{
-        const response = await fetch(`/api/user/${user.id}`,{
-            method:'GET',
-            headers:{
-                'Content-Type':"Application/json"
-            }
-        })
+  const getUser = async () => {
+    const response = await fetch(`/api/user/${user.id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+    });
 
-        const data = await response.json()
-        setUserInfo(data)
-        setLoading(false)
+    const data = await response.json();
+    setUserInfo(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (user) {
+      getUser();
     }
+  }, [user]);
 
-    useEffect(()=>{
-        if(user){
-            getUser()
-        }
-    },[user])
+  const isFollwing = userInfo?.following?.find(
+    (item) => item._id === userData._id
+  );
 
-    const isFollwing = userInfo?.following?.find((item)=> item._id === userData._id)
+  const handleFollow = async () => {
+    const response = await fetch(
+      `/api/user/${user.id}/follow/${userData._id}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    setUserInfo(data);
+    update();
+  };
 
-    const handleFollow = async()=>{
-        const response = await fetch(`/api/user/${user.id}/follow/${userData._id}`,{
-          method:'POST',
-          headers:{
-            "Content-Type":"application/json"
-          },
-        })
-        const data = await response.json()
-        setUserInfo(data)
-        update()
-    }
+  return loading || !isLoaded ? (
+    <Loader />
+  ) : (
+    <div className="flex justify-between items-center">
+      <Link className="flex gap-4 items-center" href={`/profile/${userData._id}/posts`}>
+        <Image
+          src={userData.profilePhoto}
+          alt="profile pic"
+          width={50}
+          height={50}
+          className="rounded-full"
+        />
+        <div className="flex flex-col gap-1">
+          <p className="text-small-semibold text-light-1">
+            {userData.firstName} {userData.lasttName}
+          </p>
+          <p className="text-subtle-medium textlight-3">@{userData.username}</p>
+        </div>
+      </Link>
 
-  return loading || !isLoaded ? <Loader/> : (
-    <div className='flex justify-between items-center'>
-        <div className='flex gap-4 items-center'>
-            <Image src={userData.profilePhoto} alt='profile pic' width={50} height={50} className='rounded-full'/>
-            <div className='flex flex-col gap-1'>
-                <p className='text-small-semibold text-light-1'>{userData.firstName} {userData.lasttName}</p>
-                <p className='text-subtle-medium textlight-3'>@{userData.username}</p>
-            </div>
-        </div>  
-
-        {user.id !== userData.clerkId && (
-
-            isFollwing ? (
-                <PersonRemove sx={{color:'#7857ff',cursor:'pointer'}} onClick={()=>handleFollow()}/>
-
-            ):(
-                <PersonAddAlt sx={{color:'#7857ff',cursor:'pointer'}} onClick={()=>handleFollow()}/>
-            )           
-        )}
-
+      {user.id !== userData.clerkId &&
+        (isFollwing ? (
+          <PersonRemove
+            sx={{ color: "#7857ff", cursor: "pointer" }}
+            onClick={() => handleFollow()}
+          />
+        ) : (
+          <PersonAddAlt
+            sx={{ color: "#7857ff", cursor: "pointer" }}
+            onClick={() => handleFollow()}
+          />
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default UserCard
+export default UserCard;
